b9e98fb81996374615b6a5102f76764948c5e726
Ocsigen Buildbot <dev@ocsigen.org>
2016-05-20 08:07:46 +0000

    js_of_ocaml-cfb4c9b

--- js_of_ocaml/dev/manual/files/toplevel/test_dynlink.js
+++ js_of_ocaml/dev/manual/files/toplevel/test_dynlink.js
@@ -1,11 +1,13 @@
 (function(joo_global_object)
    {"use strict";
-    var runtime=joo_global_object.jsoo_runtime;
+    var
+     runtime=joo_global_object.jsoo_runtime,
+     caml_new_string=runtime.caml_new_string;
     function caml_call1(f,a0)
      {return f.length==1?f(a0):runtime.caml_call_gen(f,[a0])}
     var
      global_data=runtime.caml_get_global_data(),
-     cst_Dynlink_OK=runtime.caml_new_string("Dynlink OK"),
+     cst_Dynlink_OK=caml_new_string("Dynlink OK"),
      Pervasives=global_data.Pervasives;
     caml_call1(Pervasives[31],cst_Dynlink_OK);
     var Test_dynlink=[0];
